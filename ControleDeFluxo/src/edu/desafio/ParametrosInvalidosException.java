517ac3f5975739bd98b87f69bb2e32e90f41d0fe
Guilherme <50538146+guilhermerek@users.noreply.github.com>
2024-08-19 21:08:38 -0300

    desafio Controle de Fluxo

---
+++ ControleDeFluxo/src/edu/desafio/ParametrosInvalidosException.java
@@ -0,0 +1,10 @@
+package edu.desafio;
+
+public class ParametrosInvalidosException extends Exception {
+	/**
+	 * assim como nos videos visto da sintaxe basica, tratamento de exceções
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+}
